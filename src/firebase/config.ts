84a8efe81ad27ddb0dfa200f3cab07bebcedbffa
Ajay Dnex <ajay.dnex@gmail.com>
2026-02-16 05:45:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3110244339-6cbfd",
+  "appId": "1:1044763054467:web:185a1021692dc2865ea6b6",
+  "apiKey": "AIzaSyBwXLoueltbX-BXsSOfu138XGNGVUjiy5k",
+  "authDomain": "studio-3110244339-6cbfd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1044763054467"
+};
